f297a8e09fe43002f4d2a814b8b66980a6d5c80f
Dmitry <102182246+WorkHotRoad@users.noreply.github.com>
2023-02-21 14:15:59 +0300

    Update selen.py

--- selen.py
+++ selen.py
@@ -1,10 +1,10 @@
 from requests_html import HTMLSession
-from auth import BASE_DIR, get_cookies
+from cooki import BASE_DIR, get_cookies
 import re
 import pickle
 from datetime import datetime
 from utils import log_text, log_error, get_times_list, format_time, format_date,  MAIN_URL
-from urllib.parse import urljoin
+# from urllib.parse import urljoin
 
 
 def add_data(url, times_list, session):
@@ -17,13 +17,16 @@ def add_data(url, times_list, session):
         text =(" ".join(text))
         #выбираем все года, где нет изменений
         # pattern = r'(\d{4}|\d{4}\s\—\s\d{4}).{5,30}UTC ([\+\-]\d.{0,7}).{0,1}h'
-        pattern = r'(\d{4}|\d{4}\s\—\s\d{4}).{5,30}UTC ([\+\-]\d.{0,7}) h'
+        # pattern = r'(\d{4}|\d{4}\s\—\s\d{4}).{5,30}UTC ([\+\-]\d.{0,7}) h'
+        pattern = r'(\d{4}|\d{4}\s\—\s\d{4}).{,30}UTC ([\+\-]\d{,2}\:{,1}\d{,2}\:{,1}\d{,2}).{,1}h'
         result1 = re.findall(pattern, text)
         #выбираем все года, где есть изменения
         # pattern = r'(\d{8}.\d{4}).{5,50}UTC([\+\-]\d.{0,7})h'
-        pattern = r'(\d{8}.\d{4}).{5,50}UTC([\+\-]\d.{0,7})h'
+        # pattern = r'(\d{8}.\d{4}).{5,50}UTC([\+\-]\d.{0,7})h'
+        # pattern = r'(\d{8}.\d{4}).{5,50}([\+\-]\d.{0,7}).{0,1}h'
+        pattern = r'(\d{8}.\d{4}).{,50}([\+\-]\d{,2}\:{,1}\d{,2}\:{,1}\d{,2}).{,1}h'
         result2 = re.findall(pattern, text)
-        res += result1 + result2
+        res += result2 + result1
     return res
 
 def main():
@@ -43,7 +46,7 @@ def main():
         asession.cookies.set(cookie['name'], cookie['value'])
 
     start_time = datetime.now()
-    for number in range(1, 2):
+    for number in range(3875, 3876):
         result = []
         response = asession.get(MAIN_URL, params = {"n":number})
         if not response:
@@ -52,42 +55,49 @@ def main():
         # получаем url города
         url = response.html.url
         # забираем имя города, страны,
-        pattern_for_plase = r"Time Zone in (.+)"
+        pattern_for_plaсe = r"Time Zone in (.+)"
         table = response.html.xpath(
                     '//h1[@class="headline-banner__title"]/text()'
                 )
         place = re.search(
-            pattern_for_plase, " ".join(table).replace(", ", "_")
+            pattern_for_plaсe, " ".join(table).replace(", ", "_")
         ).group(1)
         # создаем директорию для файлов
         results_dir = BASE_DIR/'results'
-        results_dir.mkdir(parents=True, exist_ok=True)
-        # получаем все временные промежутки
-        times_list = get_times_list(response)
-        # получаем данные с нужными годами, сортируем и форматируем их
-        res = add_data(url, times_list, asession)
-        sort_res = sorted(
-            [[format_date(x[0]), format_time(x[1])] for x in res], key =lambda x: x[0]
-        )
-        # удаляем дубли и объединяем данные
-        if sort_res:
-            ready_data = [sort_res[0]]
-            for i in range(1, len(sort_res)):
-                if ready_data[-1][1] != sort_res[i][1]:
-                    ready_data.append(sort_res[i])
-            ready_data = ["|".join(x) for x in ready_data]
-            result += ready_data
-            if len(result) <2 :
-                log_text(number, "нашли только 1но изменение, проверьте")
+        print(place.lower())
+        if 'time zone' not in place.lower():
+            results_dir.mkdir(parents=True, exist_ok=True)
+            # получаем все временные промежутки
+            times_list = get_times_list(response)
+            # получаем данные с нужными годами, сортируем и форматируем их
+            res = add_data(url, times_list, asession)
+            sort_res = sorted(
+                [[format_date(x[0]), format_time(x[1])] for x in res], key =lambda x: x[0]
+            )
+            # удаляем дубли и объединяем данные
+            if sort_res:
+                ready_data = [sort_res[0]]
+                for i in range(1, len(sort_res)):
+                    if ready_data[-1][1] != sort_res[i][1]:
+                        ready_data.append(sort_res[i])
+                ready_data = ["|".join(x) for x in ready_data]
+                result += ready_data
+                if len(result) <2 :
+                    log_text(number, "нашли только 1но изменение, проверьте")
+            else:
+                log_text(number, "не нашли изменений")
+                result += ["No changes"]
+            # сохраняем в фаил
+
+            num_for_file = format(number, '0>4n')
+            file_path = f"{results_dir}/{num_for_file}_{place}.txt"
+            with open(file_path, "w") as f:
+                f.writelines(x+"\n" for x in result)
         else:
-            log_text(number, "не нашли изменений")
-            result[0] = "No changes"
-        # сохраняем в фаил
-        num_for_file = format(number, '0>4n')
-        file_path = f"{results_dir}/{num_for_file}_{place}.txt"
-        with open(file_path, "w") as f:
-            f.writelines(x+"\n" for x in result)
+            log_text(number, r"страница 'Time Zone' или 'Military Time'")
+
     print("парсер закончил работу")
+    print("Проверьте log_file")
     print(datetime.now() - start_time )
 
 
